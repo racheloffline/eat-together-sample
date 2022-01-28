a1cec93852bd220289ac80d2811d15fed4c854a6
Rachel Hu <39543727+Rachelonline@users.noreply.github.com>
2022-01-28 15:29:51 -0800

    Created basic template file, adding docs soon.

---
+++ src/navigation/AppNavigator.js
@@ -0,0 +1,130 @@
+import React, { useContext } from "react";
+import firebase from "firebase/app";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import { useTheme, themeColor } from "react-native-rapi-ui";
+import TabBarIcon from "../components/utils/TabBarIcon";
+import TabBarText from "../components/utils/TabBarText";
+//Screens (Make sure to import if ever adding new screen!)
+import Home from "../screens/Home";
+import SecondScreen from "../screens/SecondScreen";
+import Chat from "../screens/Chat";
+import Profile from "../screens/Profile";
+import Loading from "../screens/utils/Loading";
+// Auth screens
+import Login from "../screens/auth/Login";
+import Register from "../screens/auth/Register";
+import ForgetPassword from "../screens/auth/ForgetPassword";
+import { AuthContext } from "../provider/AuthProvider";
+
+// Better put your these secret keys in .env file
+const firebaseConfig = {
+  apiKey: "AIzaSyDYuhOpbDxlVHBKxVz6gW45eyutD26AsGg",
+  authDomain: "eat-together-303ec.firebaseapp.com",
+  databaseURL: "https://eat-together-303ec.firebaseio.com",
+  projectId: "eat-together-303ec",
+  storageBucket: "eat-together-303ec.appspot.com",
+  messagingSenderId: "856869460838",
+  appId: "1:856869460838:web:01e0197a0abc9fffb686a7",
+};
+
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+const AuthStack = createStackNavigator();
+const Auth = () => {
+  return (
+    <AuthStack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <AuthStack.Screen name="Login" component={Login} />
+      <AuthStack.Screen name="Register" component={Register} />
+      <AuthStack.Screen name="ForgetPassword" component={ForgetPassword} />
+    </AuthStack.Navigator>
+  );
+};
+
+const MainStack = createStackNavigator();
+const Main = () => {
+  return (
+    <MainStack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <MainStack.Screen name="MainTabs" component={MainTabs} />
+      <MainStack.Screen name="SecondScreen" component={SecondScreen} />
+    </MainStack.Navigator>
+  );
+};
+
+const Tabs = createBottomTabNavigator();
+const MainTabs = () => {
+  const { isDarkmode } = useTheme();
+  return (
+    <Tabs.Navigator
+      tabBarOptions={{
+        style: {
+          borderTopWidth: 1,
+          borderTopColor: isDarkmode ? themeColor.dark100 : "#c0c0c0",
+          backgroundColor: isDarkmode ? themeColor.dark200 : "#ffffff",
+        },
+      }}
+    >
+      {/* these icons using Ionicons */}
+      <Tabs.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarLabel: ({ focused }) => (
+            <TabBarText focused={focused} title="Home" />
+          ),
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon focused={focused} icon={"md-home"} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarLabel: ({ focused }) => (
+            <TabBarText focused={focused} title="Profile" />
+          ),
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon focused={focused} icon={"person"} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Chat"
+        component={Chat}
+        options={{
+          tabBarLabel: ({ focused }) => (
+            <TabBarText focused={focused} title="Chat" />
+          ),
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon focused={focused} icon={"ios-information-circle"} />
+          ),
+        }}
+      />
+    </Tabs.Navigator>
+  );
+};
+
+export default () => {
+  const auth = useContext(AuthContext);
+  const user = auth.user;
+  return (
+    <NavigationContainer>
+      {user == null && <Loading />}
+      {user == false && <Auth />}
+      {user == true && <Main />}
+    </NavigationContainer>
+  );
+};
