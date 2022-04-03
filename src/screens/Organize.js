349d7f7db2b94c320db5da2295e6c4e93a99e14a
Rachel Hu <39543727+Rachelonline@users.noreply.github.com>
2022-04-02 17:52:08 -0700

    Basic framework for fillable form

--- src/screens/Organize.js
+++ src/screens/Organize.js
@@ -1,41 +1,73 @@
 //Functionality TDB, most likely to be used to implement ice-breaker games
 
-import React from "react";
-import { View } from "react-native";
-import {
-  Layout,
-  TopNav,
-  Text,
-  themeColor,
-  useTheme,
-} from "react-native-rapi-ui";
-import { Ionicons } from "@expo/vector-icons";
+import React, {useState} from "react";
+import {Button, Layout, Section, SectionImage, Text} from "react-native-rapi-ui";
+import { TextInput } from 'react-native-rapi-ui';
+import {View} from "react-native";
+import {Ionicons} from "@expo/vector-icons";
 
 export default function ({ navigation }) {
-  const { isDarkmode } = useTheme();
-  return (
-    <Layout>
-      <TopNav
-        middleContent="Create an invitation here!"
-        leftContent={
-          <Ionicons
-            name="chevron-back"
-            size={20}
-            color={isDarkmode ? themeColor.white100 : themeColor.black}
-          />
-        }
-        leftAction={() => navigation.goBack()}
-      />
-      <View
-        style={{
-          flex: 1,
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        {/* This text using ubuntu font */}
-        <Text fontWeight="bold">Create an invitation here!</Text>
-      </View>
-    </Layout>
-  );
+    const [title, setTitle] = React.useState('');
+    const [description, setDescription] = React.useState('');
+    const [date, setDate] = React.useState('');
+    const [time, setTime] = React.useState('');
+    const [location, setLocation] = React.useState('');
+    return (
+        <Layout>
+            <Section>
+                <SectionImage source={require('../../assets/food.jpg')} />
+            <View>
+                <TextInput
+                    placeholder="Title"
+                    value={title}
+                    onChangeText={(val) => setTitle(val)}
+                    leftContent={
+                        <Ionicons name="chatbubble-outline" size={20} />
+                    }
+                />
+            </View>
+            <View>
+                <TextInput
+                    placeholder="Description"
+                    value={description}
+                    onChangeText={(val) => setDescription(val)}
+                    leftContent={
+                        <Ionicons name="document-text-outline" size={20}/>
+                    }
+                />
+            </View>
+                <View>
+                    <TextInput
+                        placeholder="Date"
+                        value={date}
+                        onChangeText={(val) => setDate(val)}
+                        leftContent={
+                            <Ionicons name="calendar-outline" size={20}/>
+                        }
+                    />
+                </View>
+                <View>
+                    <TextInput
+                        placeholder="Time"
+                        value={time}
+                        onChangeText={(val) => setTime(val)}
+                        leftContent={
+                            <Ionicons name="time-outline" size={20}/>
+                        }
+                    />
+                </View>
+                <View>
+                    <TextInput
+                        placeholder="Location"
+                        value={location}
+                        onChangeText={(val) => setLocation(val)}
+                        leftContent={
+                            <Ionicons name="location-outline" size={20}/>
+                        }
+                    />
+                </View>
+                <Button text="Post" status="success" />
+            </Section>
+        </Layout>
+    );
 }
