c0fd596ca6e5e12b3df91c15df9cdd61b412f5db
Rachel Hu <39543727+Rachelonline@users.noreply.github.com>
2022-05-16 23:09:51 -0700

    Added some comments for matching algo

--- src/methods.js
+++ src/methods.js
@@ -1,6 +1,24 @@
+import {db} from "./provider/Firebase";
+import firebase from "firebase";
+
+/*
+Generates a random color.
+Returns: Hex value of random color.
+ */
 export const generateColor = () => {
     const randomColor = Math.floor(Math.random() * 16777215)
         .toString(16)
         .padStart(6, '0');
     return `#${randomColor}`;
 };
+
+/*
+TODO: ELAINE
+Gets a list of the top 5 profiles suggestions based on the current user and tags.
+Returns: A list of usernames of the top 5 user suggestions.
+ */
+export const getProfileRecs = () => {
+    const user = firebase.auth().currentUser;
+    const allUsers = db.collection("Users");
+
+}
