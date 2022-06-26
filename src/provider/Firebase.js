7f9f2740442256cecfa1e2fe1d5e2cb80a779556
Rachel Hu <39543727+Rachelonline@users.noreply.github.com>
2022-06-25 18:58:55 -0700

    privacy

--- src/provider/Firebase.js
+++ src/provider/Firebase.js
@@ -3,13 +3,13 @@ import firebase from "firebase";
 //Connect to firebase
 
 export const firebaseConfig = {
-    apiKey: "AIzaSyDYuhOpbDxlVHBKxVz6gW45eyutD26AsGg",
-    authDomain: "eat-together-303ec.firebaseapp.com",
-    databaseURL: "https://eat-together-303ec.firebaseio.com",
-    projectId: "eat-together-303ec",
-    storageBucket: "eat-together-303ec.appspot.com",
-    messagingSenderId: "856869460838",
-    appId: "1:856869460838:web:01e0197a0abc9fffb686a7",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
 }
 
 if (firebase.apps.length === 0) {
