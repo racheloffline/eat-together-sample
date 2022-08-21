d20d75699f690f79b5105ceebe5f7db3cc2be089
Rachel Hu <39543727+Rachelonline@users.noreply.github.com>
2022-08-20 21:52:44 -0700

    fix bottom navigation bar weird spacing

--- src/components/utils/TabBarIcon.js
+++ src/components/utils/TabBarIcon.js
@@ -5,25 +5,22 @@ import { Layout, Text, themeColor, useTheme } from "react-native-rapi-ui";
 import { Ionicons } from "@expo/vector-icons";
 
 export default (props) => {
-  const { isDarkmode } = useTheme();
   return (
-    <Layout style={{justifyContent: "space-between", margin: 2, alignItems: "center"}} backgroundColor="white">
+    <Layout
+      style={{
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+      backgroundColor="white"
+    >
       <Ionicons
         name={props.icon}
-        style={{ marginBottom: -7 }}
         size={24}
-        color={
-          props.focused
-            ? isDarkmode
-              ? themeColor.white100
-              : themeColor.primary200
-            : "rgb(143, 155, 179)"
-        }
+        color={props.focused ? themeColor.primary200 : "rgb(143, 155, 179)"}
       />
       <Text
         fontWeight="bold"
         style={{
-          marginBottom: 5,
           color: "rgb(143, 155, 179)",
           fontSize: 10,
         }}
