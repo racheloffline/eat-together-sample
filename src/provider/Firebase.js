3a587d969d84579b5a97fc2647b3631b386ad4a1
Rachel Hu <39543727+Rachelonline@users.noreply.github.com>
2022-08-20 23:38:56 -0700

    Added back .env security

--- src/provider/Firebase.js
+++ src/provider/Firebase.js
@@ -1,23 +1,23 @@
 import firebase from "firebase";
-//import {
-//    FIREBASE_API_KEY,
-//    FIREBASE_AUTH_DOMAIN,
-//    FIREBASE_DATABASE_URL,
-//    FIREBASE_PROJECT_ID,
-//    FIREBASE_STORAGE_BUCKET,
-//    FIREBASE_MESSAGING_SENDER_ID,
-//    FIREBASE_APP_ID,
-//} from "@env"; //Enviroment variables
+import {
+    FIREBASE_API_KEY,
+    FIREBASE_AUTH_DOMAIN,
+    FIREBASE_DATABASE_URL,
+    FIREBASE_PROJECT_ID,
+    FIREBASE_STORAGE_BUCKET,
+    FIREBASE_MESSAGING_SENDER_ID,
+    FIREBASE_APP_ID,
+} from "@env"; //Enviroment variables
 
 //Connect to firebase
 const firebaseConfig = {
- apiKey: "AIzaSyDYuhOpbDxlVHBKxVz6gW45eyutD26AsGg",
- authDomain: "eat-together-303ec.firebaseapp.com",
- databaseURL: "https://eat-together-303ec.firebaseio.com",
- projectId: "eat-together-303ec",
- storageBucket: "eat-together-303ec.appspot.com",
- messagingSenderId: "856869460838",
- appId: "1:856869460838:web:01e0197a0abc9fffb686a7",
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  databaseURL: FIREBASE_DATABASE_URL,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
 };
 
 
