9cfb40e00d151ca0ce15dd679414d26843d70033
Scott <scottdangrem@gmail.com>
2022-12-31 01:34:46 -0800

    Changed backgroundColor default to white, border radius on by default, and default text input color to black

--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -8,8 +8,8 @@ function TextInput(props) {
     let [fontsLoaded] = useFonts({ Inter_600SemiBold });
     const value = props.value ? props.value : "";
     const placeholder = props.placeholder ? props.placeholder : "";
-    const color = props.color ? props.color : "white";
-    const fontSize = props.fontSize ? props.fontSize : 17;
+    const color = props.color ? props.color : "black";
+    const fontSize = props.fontSize ? props.fontSize : 15;
     const fontFamily = fontsLoaded ? "Inter_600SemiBold" : (Platform.OS === "ios" ? "AppleSDGothicNeo-Medium" : "sans-serif-medium");
 
     const onChangeText = props.onChangeText ? props.onChangeText : () => {};
@@ -17,13 +17,13 @@ function TextInput(props) {
   
     // Affects the container
     // Restricts TextInput into a fixed rectangle defined by width and height
-    const backgroundColor = props.backgroundColor ? props.backgroundColor : "#5DB075";
-    const borderColor = props.borderColor ? props.borderColor : "";
-    const borderWidth = props.borderWidth ? props.borderWidth : "";
+    const backgroundColor = props.backgroundColor ? props.backgroundColor : "white";
+    const borderColor = props.borderColor ? props.borderColor : "lightgrey";
+    const borderWidth = props.borderWidth ? props.borderWidth : "1%";
     const width = props.width ? props.width : "30%";
     const minWidth = width;
     const maxWidth = width;
-    const height = props.height ? props.height : "5%";
+    const height = props.height ? props.height : "7%";
     const minHeight = height;
     const maxHeight = height;
    
@@ -39,64 +39,65 @@ function TextInput(props) {
     const iconRightOnPress = props.iconRightOnPress ? props.iconRightOnPress : () => {};
 
     const styles = StyleSheet.create({
-      textInput: {
-        flex: 1,
-        margin: "0%",
-        paddingHorizontal: "3%",
-        
-        // Placeholder is top left of TextInput if multiline, else centered (single line)
-        textAlignVertical: multiline ? "top" : "center",
-        
-        fontWeight: "bold",
-        fontSize: fontSize,
-        fontFamily: fontFamily,
-        color: color,
+        textInput: {
+            flex: 1,
+            margin: "0%",
+            paddingHorizontal: "3%",
+            
+            // Placeholder is top left of TextInput if multiline, else centered (single line)
+            textAlignVertical: multiline ? "top" : "center",
+            
+            fontWeight: "bold",
+            fontSize: fontSize,
+            fontFamily: fontFamily,
+            color: color,
       },
-      mainContainer: {
-        flexDirection: 'row', 
-        minWidth: minWidth,
-        minHeight: minHeight,
-        maxWidth: maxWidth,
-        maxHeight: maxHeight,
-        backgroundColor: backgroundColor,
-        borderRadius: "10%",
-        borderColor: borderColor,
-        borderWidth: borderWidth,
-      }, leftContainer: {
-        display: displayLeftIcon,
-        fontSize: iconLeftFontSize,
-        marginLeft: "3%",
-        justifyContent: "center",
-      }, rightContainer: {
-        display: displayRightIcon,
-        fontSize: iconRightFontSize,
-        marginRight: "3%",
-        justifyContent: "center",
+        mainContainer: {
+            flexDirection: 'row', 
+            minWidth: minWidth,
+            minHeight: minHeight,
+            maxWidth: maxWidth,
+            maxHeight: maxHeight,
+            backgroundColor: backgroundColor,
+            borderRadius: "10%",
+            borderColor: borderColor,
+            borderWidth: borderWidth,
+      }, 
+        leftContainer: {
+            display: displayLeftIcon,
+            fontSize: iconLeftFontSize,
+            marginLeft: "3%",
+            justifyContent: "center",
+      }, 
+        rightContainer: {
+            display: displayRightIcon,
+            fontSize: iconRightFontSize,
+            marginRight: "3%",
+            justifyContent: "center",
       },
     });
   
     return (
-    <View style={styles.mainContainer}>
-        <View style={styles.leftContainer}>
-            <Ionicons name={iconLeft} />
+        <View style={styles.mainContainer}>
+            <View style={styles.leftContainer}>
+                <Ionicons name={iconLeft} />
+            </View>
+            <ReactNativeTextInput 
+                style={styles.textInput}
+                onChangeText={onChangeText}
+                placeholder={placeholder}
+                placeholderTextColor={"darkgrey"}
+                autoCapitalize={false}
+                value={value}
+                multiline={multiline}
+                blurOnSubmit={true}
+                onSubmitEditing={(e) => {Keyboard.dismiss(); onSubmitEditing(e);} }
+            />
+            <TouchableOpacity onPress={iconRightOnPress} style={styles.rightContainer}>
+                <Ionicons name={iconRight} />
+            </TouchableOpacity>
         </View>
-        <ReactNativeTextInput 
-        style={styles.textInput}
-        onChangeText={onChangeText}
-        placeholder={placeholder}
-        placeholderTextColor={"rgba(255, 255, 255, .8)"}
-        autoCapitalize={false}
-        value={value}
-        multiline={multiline}
-        blurOnSubmit={true}
-        onSubmitEditing={(e) => {Keyboard.dismiss(); onSubmitEditing(e);} }
-        >
-        </ReactNativeTextInput>
-        <TouchableOpacity onPress={iconRightOnPress} style={styles.rightContainer}>
-            <Ionicons name={iconRight} />
-        </TouchableOpacity>
-    </View>
-    )
+    );
   }
 
   export default TextInput;
