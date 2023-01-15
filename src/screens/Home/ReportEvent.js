c04268ddece40f48555d16e2da0d44adc7d6f437
Scott <scottdangrem@gmail.com>
2023-01-15 00:14:14 -0800

    Replaced TextInputs for ReportPerson and ReportEvent screens

--- src/screens/Home/ReportEvent.js
+++ src/screens/Home/ReportEvent.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import {View, StyleSheet, Image, Dimensions, ScrollView} from "react-native";
-import {Layout, TopNav, Button, TextInput} from "react-native-rapi-ui";
+import {Layout, TopNav, Button} from "react-native-rapi-ui";
+import TextInput from "../../components/TextInput";
 import LargeText from "../../components/LargeText";
 import MediumText from "../../components/MediumText";
 import {FontAwesome, Ionicons} from "@expo/vector-icons";
@@ -27,7 +28,15 @@ const ReportEvent = ({ route, navigation }) => {
                 <View style={styles.header}>
                     <LargeText style={{padding: 20}} center>We're sorry for your unfortunate experience. Please describe the issue.</LargeText>
                 </View>
-                <TextInput multiline={true} containerStyle={{paddingBottom: 70}} placeholder="Enter explanation here" value={report} onChangeText={val => setReport(val)}/>
+                <TextInput
+                    multiline={true}
+                    mainContainerStyle={{alignItems: "flex-start"}}
+                    width="100%"
+                    height={130}
+                    placeholder="Enter explanation here"
+                    value={report}
+                    onChangeText={(val) => setReport(val)}
+                />
                 <Button style={{marginTop: 20}} text="Report" status="danger" onPress={() => {
                     admin
                         .firestore()
